1eeaeb21a9dd08f92453e2aa99b26402c53bd49e
xavier auffray <xavierauffray@hotmail.fr>
2020-02-24 17:25:24 +0100

    playing with the config

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :passengers
   devise_for :drivers
+  devise_for :passengers
   resources :journeys, only: %i[new create destroy show edit udpate index]
   resources :passengers, only: %i[new create destroy show]
   resources :resevations, only: %i[new create destroy show]
