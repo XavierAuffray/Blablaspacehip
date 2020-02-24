2cad5316d1a172104bda50e9731a591f6ef545f8
xavier auffray <xavierauffray@hotmail.fr>
2020-02-24 16:51:43 +0100

    adding devise to both driver end passenger models

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,8 @@
 class Driver < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :journeys, dependent: :destroy
   has_many :reservations, through: :journeys
   validates :name, presence: true, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :passengers
+  devise_for :drivers
+  resources :journeys, only: %i[new create destroy show edit udpate index]
+  resources :passengers, only: %i[new create destroy show]
+  resources :resevations, only: %i[new create destroy show]
 end
