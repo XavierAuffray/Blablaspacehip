0b5c791e30bc8a6396fb7ed164b921dd1300317c
xavier auffray <xavierauffray@hotmail.fr>
2020-02-26 17:43:01 +0100

    caroussel dine

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,13 @@ import "bootstrap";
 import "slick-carousel";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import $ from "jquery"
 
 
-$(".slick-carousel").slick({
-  dots: true,
-  speed: 500,
-  arrows: true
+$('.slick-carousel').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: false
 });
+
+
