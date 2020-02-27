deaaa21ccf573818bec83cb796e59e973ec5d1bd
Roger Tosbotn <rogertosb@gmail.com>
2020-02-27 16:40:48 +0100

    fixed new journey form

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_122938) do
 
   create_table "journeys", force: :cascade do |t|
     t.string "departure"
+    t.string "arrival"
     t.datetime "date"
     t.integer "price"
     t.integer "available_seats"
@@ -40,10 +41,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_122938) do
     t.string "driver_name"
     t.bigint "driver_id", null: false
     t.string "photo"
-    t.bigint "planet_id"
-    t.string "arrival"
     t.index ["driver_id"], name: "index_journeys_on_driver_id"
-    t.index ["planet_id"], name: "index_journeys_on_planet_id"
   end
 
   create_table "passengers", force: :cascade do |t|
@@ -77,7 +75,6 @@ ActiveRecord::Schema.define(version: 2020_02_27_122938) do
     t.index ["passenger_id"], name: "index_reservations_on_passenger_id"
   end
 
-  add_foreign_key "journeys", "planets"
   add_foreign_key "reservations", "journeys"
   add_foreign_key "reservations", "passengers"
 end
